b4d1743fc9da75a04b7fae03dc4ef34920e24f03
roblav96 <roblav96@gmx.com>
2016-08-16 00:18:34 -0400

    sync it

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,9 +1,14 @@
 // 
 
-import {Component} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {User} from "../../shared/user/user";
 import {UserService} from "../../shared/user/user.service";
 import {Router} from "@angular/router";
+import {Page} from "ui/page";
+import {Color} from "color";
+import {View} from "ui/core/view";
+import {setHintColor} from "../../utils/hint-util";
+import {TextField} from "ui/text-field";
 
 
 
@@ -16,22 +21,49 @@ import {Router} from "@angular/router";
 
 
 
-export class LoginPage {
+export class LoginPage implements OnInit {
 
 	user: User
 	isLoggingIn = true
+	@ViewChild("container") container: ElementRef;
+	@ViewChild("email") email: ElementRef;
+	@ViewChild("password") password: ElementRef;
 
 	constructor(
 		private _userService: UserService,
-		private _router: Router
+		private _router: Router,
+		private page: Page
 	) {
 		this.user = new User();
 		this.user.email = "rob@gmx.com";
 		this.user.password = "abc123";
 	}
 
+	setTextFieldColors() {
+		let emailTextField = <TextField>this.email.nativeElement;
+		let passwordTextField = <TextField>this.password.nativeElement;
+
+		let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+		emailTextField.color = mainTextColor;
+		passwordTextField.color = mainTextColor;
+
+		let hintColor = new Color(this.isLoggingIn ? "red" : "#C4AFB4");
+		setHintColor({ view: emailTextField, color: hintColor });
+		setHintColor({ view: passwordTextField, color: hintColor });
+	}
+
+	ngOnInit() {
+		this.page.actionBarHidden = true;
+		this.page.backgroundImage = "res://bg_login";
+	}
 	toggleDisplay() {
 		this.isLoggingIn = !this.isLoggingIn;
+		this.setTextFieldColors();
+		let container = <View>this.container.nativeElement;
+		container.animate({
+			backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+			duration: 200
+		});
 	}
 	submit() {
 		if (this.isLoggingIn) {
