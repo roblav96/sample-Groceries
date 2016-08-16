f441ef4da93630eaccac97f9ec5b4592756fdb90
roblav96 <roblav96@gmx.com>
2016-08-15 20:49:42 -0400

    wardoo

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,32 @@
 import {Component} from '@angular/core'
 
-
 @Component({
 	selector: 'my-app',
 	// template: './app.component.html',
 	template: `
 		<StackLayout>
 			<Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-			<TextField hint="Email Address" keyboardType="email" autocorrect="false" autocapitalizationType="none"></TextField>
+			<TextField [(ngModel)]="email" hint="Email Address" keyboardType="email" autocorrect="false" autocapitalizationType="none"></TextField>
 			<TextField hint="Password" secure="true"></TextField>
-			<Button text="Sign in" class="submit-button"></Button>
-			<Button text="Sign up for Groceries"></Button>
+			<Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
+			<Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
 		</StackLayout>
 	`,
 	styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
 })
-export class AppComponent { }
+
+export class AppComponent {
+
+	email = "nativescriptrocks@telerik.com";
+	isLoggingIn = true;
+
+	submit() {
+		console.log("You’re using: " + this.email);
+		alert("You’re using: " + this.email);
+	}
+	toggleDisplay() {
+		this.isLoggingIn = !this.isLoggingIn;
+	}
+
+}
+
